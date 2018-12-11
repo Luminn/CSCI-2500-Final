a64cbfdebba00f7e2e2dafa68ecad9ad8f06f2e1
luminox <lum6@rpi.edu>
2018-12-11 16:52:04 -0500

    changes

---
+++ p1.py
@@ -0,0 +1,12 @@
+import forward_no_label
+import input
+import output
+import os
+import sys
+
+if len(sys.argv) != 3:
+    print("Invalid Arguments.")
+
+is_forwarding = True if sys.argv[1] == "F" else False
+input_file = sys.argv[2]
+
