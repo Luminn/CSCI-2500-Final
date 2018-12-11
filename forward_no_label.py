3c3ad13bd54e3c6bd7f5a1e0c23591b47da7851f
Grande-zhu <37521324+Grande-zhu@users.noreply.github.com>
2018-12-11 15:57:45 -0500

    Update forward_no_label.py
    minor changes

--- forward_no_label.py
+++ forward_no_label.py
@@ -1,14 +1,28 @@
 
+def get_label_num():
+    '''
+    A function that get number of label
+    '''   
+    label_num = 0
+    return label_num
+
+def label_position():
+    '''
+    A function that get label position
+    '''   
+    label_from = 2
+    label_to = 5
+    return (label_from,label_to)
+
 def print_cycle(cycle, cycle_num, ins_num, ins_list): 
     '''
     A function that do the print cycle
     '''
-    print("----------------------------------------------------------------------------------")
+    print("-"*82)
     print("CPU Cycles ===>     1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16") 
-    line_num = 0 
+    line_num = 0 #row num of cycle
     while((line_num < cycle_num) and (line_num < ins_num)): 
-        ins_place = 0 
-        
+        ins_place = 0 #column num of cycle
         line = ins_list[line_num] + " " * (20 - len(ins_list[line_num]))
         while (ins_place < 16): 
             if(cycle[line_num][ins_place] == 0): 
@@ -23,13 +37,14 @@ def print_cycle(cycle, cycle_num, ins_num, ins_list):
                 line += "MEM " 
             elif(cycle[line_num][ins_place] == 5): 
                 line += "WB  " 
-            elif(cycle[line_num][ins_place] == 6): 
+            elif(cycle[line_num][ins_place] == -1): 
                 line += "*   " 
             else: 
                 line+= "????" 
-            ins_place += 1 
+            ins_place += 1 #increrment for column
+        
+        line_num += 1 #increrment for row
         print(line)
-        line_num += 1 
         
 def print_register(register_list): 
     '''
@@ -53,29 +68,30 @@ def get_ins():
     ins_list = ["$s1,$zero,451", "$t2,$s0,73", "$t4,$s3,$s7"] 
     return ins_list 
 
-def get_register():    
+def get_register(register_list):    
     '''
     A function that update the register, and store in a list
     '''
     #TODO
-    return_list = [0]*18 
+    return_list = register_list
     return return_list
 
 def forward_no_label(): 
     '''
-    A function that fo the forwarding with no label
+    A function that do the forwarding with no label
     '''    
     ins_list = get_ins() #get ins
     ins_num = len(ins_list) #num of ins
     cycle = [] #main double list
     finished_ins = 0 #num of finished ins
+    register_list = [0]*18
     #build the main double list
     for i in range(16): 
-        each_ins = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] 
+        each_ins = [0]*16 
         cycle.append(each_ins)
-    #start with cycle 1 and IF    
+    print("START OF SIMULATION (forwarding)")
+    #start with cycle 1    
     cycle_num = 1 
-    cycle[0][0] = 1 
     
     #main loop
     while ((cycle_num < 16) and (finished_ins != ins_num)): 
@@ -84,11 +100,57 @@ def forward_no_label():
                 cycle[j][cycle_num - 1] = cycle_num - j 
                 if(cycle_num - j == 5): #WB finished this ins 
                     finished_ins += 1
-                    
-        register_list = get_register()   #get register updated          
+                    register_list = get_register(register_list) #get register updated
+                                     
         print_cycle(cycle, cycle_num, ins_num, ins_list) #print cycle
         print_register(register_list) #print register
         cycle_num += 1 #increment for cycle
+    print("-"*82)
+    print("END OF SIMULATION")
+
+def forward_with_one_label(): 
+    '''
+    A function that do the forwarding with label
+    '''    
+    #TODO
+    ins_list = get_ins() #get ins
+    ins_num = len(ins_list) #num of ins
+    cycle = [] #main double list
+    finished_ins = 0 #num of finished ins
+    register_list = [0]*18
+    label_from,label_to=label_position()
+    #build the main double list
+    for i in range(16): 
+        each_ins = [0]*16 
+        cycle.append(each_ins)
+    print("START OF SIMULATION (forwarding)")
+    #start with cycle 1    
+    cycle_num = 1 
+    
+    #main loop
+    while ((cycle_num < 16) and (finished_ins != ins_num)): 
+        for j in range(ins_num): 
+            if((cycle_num - j) >= 0)and((cycle_num - j) <= 5 and (j < label_from) ):
+            # for the instruction before label
+                cycle[j][cycle_num - 1] = cycle_num - j 
+                if(cycle_num - j == 5): #WB finished this ins 
+                    finished_ins += 1
+                    register_list = get_register(register_list) #get register updated
+    
+                
+                    
+                                     
+        print_cycle(cycle, cycle_num, ins_num, ins_list) #print cycle
+        print_register(register_list) #print register
+        cycle_num += 1 #increment for cycle
+    print("-"*82)
+    print("END OF SIMULATION")
         
-if __name__ == "__main__": 
-    forward_no_label()
+if __name__ == "__main__":
+    label_num = get_label_num()
+    if label_num == 0:
+        forward_no_label()
+    elif label_num == 1:
+        forward_with_one_label()
+    else:
+        print("got fvcked")
